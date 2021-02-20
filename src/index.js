2e9e4d75d0f4d23063c3f424f1ff71dba363ef6f
pconnor88 <pete@peteconnor.co.uk>
2021-02-20 14:57:04 +0000

    installing blueprint

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './pages/App';
+import '@blueprintjs/core/lib/css/blueprint.css';
+import { FocusStyleManager } from "@blueprintjs/core";
+
+FocusStyleManager.onlyShowFocusOnTabs();
 
 ReactDOM.render(
   <React.StrictMode>
--- src/pages/App.js
+++ src/pages/App.js
@@ -1,12 +1,23 @@
 import './App.css';
+import { Navbar, NavbarDivider, NavbarGroup, Button, Alignment } from "@blueprintjs/core";
 
 function App() {
   return (
     <div className="App">
+
+      <Navbar>
+          <Navbar.Group align={Alignment.LEFT}>
+              <Navbar.Heading>Tacho Viewer</Navbar.Heading>
+          </Navbar.Group>
+          <Navbar.Group align={Alignment.RIGHT}>
+              <Button className="bp3-minimal" icon="menu" />
+          </Navbar.Group>
+      </Navbar>
+
       <header className="App-header">
         
         <p>
-          Home
+          Homepage
         </p>
         
       </header>
